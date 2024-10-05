06ad337cf598c083368879768c2520c8acac3727
shahidullah177 <mdshahidsumon177@gmail.com>
2024-10-05 21:17:24 +0600

    Card section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import Container from './Components/Container'
 import HomeBanner from './Components/Home/HomeBanner'
+import {products} from '../../utils/products'
+import {truncateText} from '../../utils/truncateText'
+import ProductCard from './Components/Product/ProductCard'
+ 
 
 export default function Home() {
   return (
@@ -8,6 +12,12 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner></HomeBanner>
+        </div>
+
+        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {products.map ((product: any) =>{
+            return <ProductCard key={product.id} data= {product}></ProductCard>
+          })}
         </div>
       </Container>
     </div>
