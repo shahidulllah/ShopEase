514b87b360c46239602ef27557eae7a79b38ea1d
shahidullah177 <mdshahidsumon177@gmail.com>
2024-10-05 14:42:00 +0600

    Banner set

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import Container from './Components/Container'
+import HomeBanner from './Components/Home/HomeBanner'
 
 export default function Home() {
   return (
-   <div>
-    sdlfjls
-   </div>
+    <div className='p-8'>
+      <Container>
+        <div>
+          <HomeBanner></HomeBanner>
+        </div>
+      </Container>
+    </div>
   )
 }
