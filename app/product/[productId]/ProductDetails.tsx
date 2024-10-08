4762a4edb2480d2d446fc4b7f6e43ee7ba9280dc
shahidullah177 <mdshahidsumon177@gmail.com>
2024-10-09 02:19:07 +0600

    Details color selection

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Button from "@/app/Components/Button";
+import ProductImage from "@/app/Components/Product/ProductImage";
 import SetColor from "@/app/Components/Product/SetColor";
 import SetQuantity from "@/app/Components/Product/SetQuantity";
 import { Rating } from "@mui/material";
@@ -77,7 +78,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-            <div>dfsdf</div>
+            <ProductImage cartProduct={cartProduct} product={product} handleColorSelect={handleColorSelect}></ProductImage>
             <div className="flex flex-col gap-1 text-slate-500 text-sm">
                 <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
                 <div className="flex items-center gap-2">
@@ -114,7 +115,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
                     handleQtyIncrease={handleQtyIncrease}
                 ></SetQuantity>
                 <Horizontal></Horizontal>
-                <div>
+                <div className="max-w-[300px]">
                     <Button onclick={() => {}} label="Add To Cart"></Button>
                 </div>
             </div>
