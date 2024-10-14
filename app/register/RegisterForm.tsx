cb9374f65e8703a1f17b37d80e2dd87c9614c414
shahidullah177 <mdshahidsumon177@gmail.com>
2024-10-14 09:28:49 +0600

    Sing in api

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,7 +7,10 @@ import Input from "../Components/inputs/Input";
 import Button from "../Components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
-
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -20,20 +23,45 @@ const RegisterForm = () => {
         }
     })
 
-    const onSubmit:SubmitHandler<FieldValues> = (data) => {
-       setIsLoading(true)
-       console.log(data);
+    const router = useRouter()
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true)
+        console.log(data);
 
-    }
+        axios.post('/api/register', data)
+            .then(() => {
+                toast.success('Account created')
+
+                signIn('credentials', {
+                    email: data.email,
+                    password: data.password,
+                    redirect: false,
+                }).then((callback) => {
+                    if (callback?.ok) {
+                        router.push('/cart');
+                        router.refresh();
+                        toast.success('Logged In')
+                    }
+
+                    if (callback?.error) {
+                        toast.error(callback.error);
+                    }
+                })
+            })
+            .catch(() => toast.error('Something went wrong'))
+            .finally(() => {
+                setIsLoading(false)
+            });
+    };
 
     return (
         <>
             <Heading title="Sign up for ShopEase" />
-            <Button 
-            outline
-            label="Sign up with Google"
-            icon={AiOutlineGoogle }
-            onClick={() => {}}
+            <Button
+                outline
+                label="Sign up with Google"
+                icon={AiOutlineGoogle}
+                onClick={() => { }}
             />
             <hr className="bg-slate-300 w-full h-px" />
 
@@ -63,7 +91,7 @@ const RegisterForm = () => {
                 type="password"
             />
 
-            <Button label={isLoading? 'Loading':'Sign Up'} onClick={handleSubmit(onSubmit)}/>
+            <Button label={isLoading ? 'Loading' : 'Sign Up'} onClick={handleSubmit(onSubmit)} />
 
             <p className="text-sm">Already have an account? <Link href='/login' className="underline">Sign In</Link></p>
         </>
