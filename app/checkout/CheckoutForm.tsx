c8e25f32a1c2601bfe98d2655c725751cc2ceb93
shahidullah177 <mdshahidsumon177@gmail.com>
2024-10-21 16:28:13 +0600

    Checkout form Incomplete

--- app/checkout/CheckoutForm.tsx
+++ app/checkout/CheckoutForm.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useCart } from "@/hooks/useCart";
-import { useStripe } from "@stripe/react-stripe-js";
+import { formatPrice } from "@/utils/formatPrice";
+import { useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
 
 interface CheckoutFormProps{
     clientSecret: string,
@@ -11,6 +13,28 @@ interface CheckoutFormProps{
 const CheckoutForm: React.FC<CheckoutFormProps> = ({clientSecret, handlePaymentSuccess}) => {
     const {cartTotalAmount, handleClearCart, handleSetPaymentIntent} = useCart()
     const stripe = useStripe()
+    const elements = useElements()
+    const [isLoading, setIsLoading] = useState(false);
+    const formatedPrice = formatPrice(cartTotalAmount)
+
+    useEffect(() => {
+        if (!stripe || !clientSecret) {
+           
+            return;
+          }
+          handlePaymentSuccess(false)
+    },[stripe, clientSecret, handlePaymentSuccess])
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+          }
+      
+          setIsLoading(true);
+      
+    }
     return (
         <div>
             
